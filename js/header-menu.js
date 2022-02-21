f0697a8098886c6eb9d1073378c620223998c4dd
WWWolkodaWWW <80260640+WWWolkodaWWW@users.noreply.github.com>
2022-02-22 00:11:25 +0100

    feat: add portfolio fix: adaptive menu

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,41 +1,43 @@
 let humburger = document.querySelector('.humburger');
 let navMobile = document.querySelector('.header__navigation_mobile');
-let back = document.querySelector('body');
-let navList = document.querySelector('.header-navigation__list');
 let links = document.querySelector('.header-navigation__link');
 let aboutContent = document.querySelector('.about-me__content');
-let overlay = document.querySelector('.about-me');
 
-function burgerMenu(selector) { 
+function burgerMenu() { 
 	humburger.addEventListener("click", (e) => {
-	humburger.classList.toggle('_active');
+		if (humburger.classList.contains('_active')) {
+			humburger.classList.remove('_active');
+			aboutContent.style.visibility = "visible";
+			navMobile.style.visibility = "hidden";
+		} else { 
+			humburger.classList.add('_active');
+			aboutContent.style.visibility = "hidden";
+			navMobile.style.visibility = "visible";
+		}
+
+		links.forEach(link => { 
+			link.addEventListener('click', () => { 
+				if (humburger.classList.contains('_active')) { 
+					humburger.classList.remove('_active');
+					aboutContent.style.visibility = "visible";
+					navMobile.style.visibility = "hidden";
+
+					links.forEach(link => { 
+						link.style.animation = `navLinkFade 1s ease forwards`;
+					})
+				}
+			})
+		})
+
 	e.preventDefault();
-	hideContent();
-	showNavigation();
 	});
 
-	function hideContent() { 
-		navMobile.classList.toggle('_active');
-		if (navMobile.classList.contains('_active')) {
-			aboutContent.style.visibility = "hidden";
-		} else { 
-			aboutContent.style.visibility = "visible";
-		}
-	}
-
-	function showNavigation() { 
-		if (navMobile.classList.contains('_active')) {
-			navMobile.style.visibility = "visible";
-		} else { 
-			navMobile.style.visibility = "hidden";
-		}
-	}
 }
 
 window.addEventListener('scroll', function() {
 	navMobile.style.visibility = "hidden";
 	aboutContent.style.visibility = "visible";
-	humburger.classList.toggle('_active');
+	humburger.classList.remove('_active');
 });
 
 burgerMenu();
